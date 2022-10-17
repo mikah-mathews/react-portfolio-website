d2022bb5a834ba3d6237e3ba894d15eb973cd861
Mikah Mathews <mikah@outofband.org>
2022-10-17 15:27:47 -0700

    Add me.png and socials header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,12 @@ const Header = () => {
         <h1>Mikah Mathews</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt='a photo of a fat white non-binary person with short blue and purple hair.' />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
