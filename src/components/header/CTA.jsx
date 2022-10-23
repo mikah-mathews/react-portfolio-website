4570c4c2fd2eb0f4eed1a72808e3157186b38ee9
Mikah Mathews <mikah@outofband.org>
2022-10-23 15:45:36 -0700

    BUG: 'me' component does not have gradient, photo is cut off

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../assets/resume.docx';
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={Resume} download className="btn">Download Resume</a>
+      <a href={Resume} download className="btn">Download CV</a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   )
