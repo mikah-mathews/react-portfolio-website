9dc97bab9749db3a5849473ad3f7a442446d33f5
Mikah Mathews <mikah@outofband.org>
2022-10-23 16:40:50 -0700

    Add navigation bar, Update components to use sections and have id's

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+import {BiUserCircle} from 'react-icons/bi'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {MdOutlineContactSupport} from 'react-icons/md'
+import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className='active'><BiHomeAlt /></a>
+      <a href='#about'><BiUserCircle /></a>
+      <a href='#experience'><BiBook /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><AiOutlineMessage /></a>
+    </nav>
   )
 }
 
